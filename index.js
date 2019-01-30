a2338d6a4aae30a95282ea94e929aff83414b87b
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-30 12:48:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function addToCart(item) {
   var itemName = [];
   var itemPrice = Math.floor((Math.random()) * 100);
   var obj = { itemName: 'bananas', itemPrice: 17};
-  var objnew = JSON.parse();
+  var objnew = JSON.parse(obj);
   cart.push(obj);
   console.log(`${itemName}`+ "has been added to your cart.");
   return cart;
